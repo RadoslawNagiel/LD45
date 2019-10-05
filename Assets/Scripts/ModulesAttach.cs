f37abd7c47ff87ca5c5e8ac0717aaab62893c4fd
Adam Domaszewski <adamdox@gmail.com>
2019-10-05 16:10:04 +0200

    Add not complete single module attachment

---
+++ Assets/Scripts/ModulesAttach.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModulesAttach : MonoBehaviour
+{
+    private Rigidbody2D selfRigidbody;
+
+    void Start()
+    {
+        selfRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("OnTriggerEnter2D");
+        GameObject module = collision.gameObject;
+
+        FixedJoint2D joint = module.AddComponent<FixedJoint2D>();
+        joint.connectedBody = selfRigidbody;
+        joint.enableCollision = false;
+
+        Destroy(module.GetComponent<Collider2D>());
+    }
+}
