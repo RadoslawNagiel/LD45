eefd200f377e77562f910754c563f75b982c0eb7
Adam Domaszewski <adamdox@gmail.com>
2019-10-05 21:00:45 +0200

    Add attaching multiple modules into dot with connecting them with each other

--- Assets/Scripts/ModulesAttach.cs
+++ Assets/Scripts/ModulesAttach.cs
@@ -4,24 +4,33 @@ using UnityEngine;
 
 public class ModulesAttach : MonoBehaviour
 {
-    public ModulesJointAttacher modulesJointFactory = new ModulesJointAttacher();
-
-    private Rigidbody2D selfRigidbody;
-
-    void Start()
-    {
-        selfRigidbody = GetComponent<Rigidbody2D>();
-    }
-
     void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("OnTriggerEnter2D");
+        Debug.Log("OnTriggerEnter2D " + GetInstanceID());
 
-        GameObject module = collision.gameObject;
-        modulesJointFactory.MakeJointBetween(module, selfRigidbody);
+        ContactPoint2D contactPoint = getFirstContactPoint(collision);
 
-        Destroy(module.GetComponent<Collider2D>());
+        GameObject objectToAttach = collision.gameObject;
+        Rigidbody2D otherRigidbody = objectToAttach.GetComponent<Rigidbody2D>();
+
+        HingeJoint2D joint = gameObject.AddComponent<HingeJoint2D>();
+        attachTo(joint, otherRigidbody);
+
+        //GetComponent<Collider2D>().isTrigger = false;
     }
 
-    
+    private ContactPoint2D getFirstContactPoint(Collider2D sourceCollider)
+    {
+        ContactPoint2D[] contactPoints = new ContactPoint2D[1];
+        sourceCollider.GetContacts(contactPoints);
+        return contactPoints[0];
+    }
+
+    private void attachTo(HingeJoint2D joint, Rigidbody2D otherRigidbody)
+    {
+        joint.connectedBody = otherRigidbody;
+        joint.autoConfigureConnectedAnchor = false;
+        joint.enableCollision = false;
+        joint.limits = new JointAngleLimits2D { min = -10, max = 10 };
+    }
 }
