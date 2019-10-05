5b0b1c37cf95f971d393d939e605508847339b52
Graup <nagiel.media@gmail.com>
2019-10-05 15:07:46 +0200

    Add Dot Controlle

---
+++ Assets/Scripts/DotMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DotMovement : MonoBehaviour
+{
+    [SerializeField] float dotSpeed = 5f; 
+
+    Vector3 pos;
+    
+    void Start()
+    {
+        pos = transform.position;
+    }
+    
+    void Update()
+    {
+        float MoveHorizontal = Input.GetAxis("Horizontal");
+        float MoveVertical = Input.GetAxis("Vertical");
+
+        pos.x += MoveHorizontal * Time.deltaTime * dotSpeed;
+        pos.z += MoveVertical * Time.deltaTime * dotSpeed;
+
+        transform.position = pos; 
+    }
+}
