18154d794423b590343518c10d3881a289e6568a
Adam Domaszewski <adamdox@gmail.com>
2019-10-05 12:45:38 +0200

    Initial commit

---
+++ Assets/Scripts/DotCreator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DotCreator : MonoBehaviour
+{
+    public GameObject dot;
+
+    private bool dotActivated;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButton(0) && !dotActivated)
+        {
+            dot.SetActive(true);
+            dotActivated = true;
+        }
+    }
+}
