63bf8a783ed4ba4d09c87bd909764ef8358b76d5
Graup <nagiel.media@gmail.com>
2019-10-06 18:29:04 +0200

    Add camera script

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+
+    private void FixedUpdate()
+    {
+        Vector3 pos = transform.position;
+        pos.x = player.transform.position.x;
+        pos.y = player.transform.position.y;
+        transform.position = pos;
+    }
+}
