686b6aff28b9af4fb9d930c688cf2f9daf7eba01
Adam Domaszewski <adamdox@gmail.com>
2019-10-07 22:43:52 +0200

    Add gradually destroying shield

---
+++ Assets/Scripts/ShieldDamageTaking.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldDamageTaking : DamageTaking
+{
+    [SerializeField] Sprite[] shieldSprites;
+
+    private SpriteRenderer renderer;
+
+    private void Start()
+    {
+        renderer = GetComponent<SpriteRenderer>();
+    }
+
+    public override void TakeDamage(int damage)
+    {
+        base.TakeDamage(damage);
+        renderer.sprite = shieldSprites[hp];
+    }
+}
