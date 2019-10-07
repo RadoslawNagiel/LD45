a52535b68ba671eefc6be216434b41c9cee92a6c
Adam Domaszewski <adamdox@gmail.com>
2019-10-07 21:18:41 +0200

    Change Dot to kinematic, remove obsolete movement logic

--- Assets/Scripts/DotMovement.cs
+++ Assets/Scripts/DotMovement.cs
@@ -21,15 +21,6 @@ public class DotMovement : MonoBehaviour
         float MoveVertical = Input.GetAxis("Vertical");
         float Rotate = Input.GetAxis("RotateAxis");
 
-        if (Math.Abs(MoveVertical) < 0.1f && Math.Abs(MoveVertical) < 0.1f)
-        {
-            myRigidbody.isKinematic = true;
-        }
-        else
-        {
-            myRigidbody.isKinematic = false;
-        }
-
         myRigidbody.velocity = new Vector2(
             MoveHorizontal * dotSpeed * Time.deltaTime,
             MoveVertical * dotSpeed * Time.deltaTime
